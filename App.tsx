8360f674a0984a2c776269c5a64e052280965d28
bigfudge99 <mayurnegi1999@gmail.com>
2022-08-29 18:55:48 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      	<StatusBar />
+		<Text>Hii</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
